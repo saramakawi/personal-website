29792af8090fa07a1be59d7830d18ff85460ad81
Sara Makawi <makawi@usc.edu>
2026-05-31 10:17:51 -0400

    Tailwind CSS Setup

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,9 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Hello World!</h1>
+          <h1 className="text-3xl font-bold underline text-blue-600">
+            Tailwind is working!
+          </h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
